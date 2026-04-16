fbe60011841d651446362fff2c52929645ba0e6a
younghoonkwon <younghoonkwon91@gmail.com>
2026-04-17 00:10:01 +0900

    feat: 채팅 농담 구현 (#2)

--- src/main/java/study/Application.java
+++ src/main/java/study/Application.java
@@ -1,4 +1,4 @@
-package gift;
+package study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(study.Application.class, args);
     }
 }
