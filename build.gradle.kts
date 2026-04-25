bdf0cc2d8cb90855db3641d9a5ca8d44cbf00732
권시연 <yeon@woowahan.com>
2026-04-25 19:46:52 +0900

    chore: AI setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework.ai:spring-ai-starter-model-google-genai")
+    implementation("org.springframework.ai:spring-ai-starter-model-bedrock")
 }
 
 kotlin {
